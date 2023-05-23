20efbc877ca5852ada9a17b6176c68d62b55bf2e
Henri Immonen <henri.immonen000@gmail.com>
2023-05-24 09:38:42 +1000

    Added component subheader

---
+++ src/components/Subheader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Subheader ({ text, },) {
+  return (
+    <div>
+      <h2 id="subheader">
+        {text}
+      </h2>
+    </div>
+  );
+}
+
+export default Subheader;
