333b041f42a157470095da1d9f88fd4748a22443
Henri Immonen <henri.immonen000@gmail.com>
2023-05-24 09:08:33 +1000

    Deleted all unused files and added first component

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div>
+      <h1 id="title">
+          Samus Aran and the female representation in video games
+      </h1>
+    </div>
+  );
+}
+
+export default Header;
